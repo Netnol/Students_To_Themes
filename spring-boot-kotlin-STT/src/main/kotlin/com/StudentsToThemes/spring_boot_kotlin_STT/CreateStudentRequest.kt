0d9d7c6820f523778961fb0c3294a30ff24c8569
Vadim Lukis <466547@niuitmo.ru>
2025-11-20 01:43:11 +0300

    Add files via upload

--- spring-boot-kotlin-STT/src/main/kotlin/com/StudentsToThemes/spring_boot_kotlin_STT/CreateStudentRequest.kt
+++ spring-boot-kotlin-STT/src/main/kotlin/com/StudentsToThemes/spring_boot_kotlin_STT/CreateStudentRequest.kt
@@ -5,8 +5,17 @@ import jakarta.validation.constraints.Size
 
 data class CreateStudentRequest(
     @field:NotBlank(message = "Name is Required")
-    @field:Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    @field:Size(max = 100, message = "Name must not exceed 100 characters")
     val name: String,
-    @field:Size(max = 2000, message = "CV text must not exceed 2000 characters")
-    val cvText: String
+    @field:NotBlank(message = "Hard skill is Required")
+    @field:Size(max = 100, message = "Hard skill must not exceed 100 characters")
+    val hardSkill: String,
+    @field:NotBlank(message = "Background is Required")
+    @field:Size(max = 2000, message = "Background text must not exceed 2000 characters")
+    val background: String,
+    @field:NotBlank(message = "Interest is Required")
+    @field:Size(max = 2000, message = "Interests text must not exceed 2000 characters")
+    val interests: String,
+    @field:Size(max = 100, message = "Time in week must not exceed 100 characters")
+    val timeInWeek: String?,
 )
--- spring-boot-kotlin-STT/src/main/kotlin/com/StudentsToThemes/spring_boot_kotlin_STT/UpdateThemeRequest.kt
+++ spring-boot-kotlin-STT/src/main/kotlin/com/StudentsToThemes/spring_boot_kotlin_STT/UpdateThemeRequest.kt
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 import java.util.UUID
 
-data class CreateThemeRequest(
+data class UpdateThemeRequest(
     @field:NotBlank(message = "Name is required")
     val name: String,
     @field:Size(max = 5000, message = "Description must not exceed 5000 characters")
